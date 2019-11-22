576db274d2b6768cc7aa1e3dd4186734e7d1e27b
Saqibur <saqibur.rahman98@gmail.com>
2019-11-22 17:11:49 +0600

    can now get a long list of previously connected mac addresses

--- DeviceCode/DeviceCode.ino
+++ DeviceCode/DeviceCode.ino
@@ -1,29 +1,32 @@
 #include <ESP8266WiFi.h>
 
 #include "./includes/APSetup.h"
+#include "./includes/MacAddress.h"
 
 // Setup the access point.
 // #TODO Make this user-specified.
 
-char* APssid = "NodeMCU";
-char* APpassword = "1234567890";
+char *APssid = "NodeMCU";
+char *APpassword = "1234567890";
+
 void setup()
 {
-  
-  // The module we are using communicates at the 9600 baud rate.
-  // Change it according to your device.
-  Serial.begin(9600);
-  
-  delay(10);
-  setupAP(APssid, APpassword);
-  Serial.println(WiFi.softAPIP());
-  
+
+    // The module we are using communicates at the 9600 baud rate.
+    // Change it according to your device.
+    Serial.begin(9600);
+    delay(1000);
+    setupAP(APssid, APpassword);
+    Serial.println(WiFi.softAPIP());
 }
 
 void loop()
 {
-  Serial.println(WiFi.softAPIP());
+    Serial.println(WiFi.softAPIP());
 
-  delay(100);
-  Serial.println();
+    delay(5000);
+    Serial.println();
+    String listOfMacs;
+    listOfMacs += showMacAddressList();
+    Serial.println(listOfMacs);
 }
