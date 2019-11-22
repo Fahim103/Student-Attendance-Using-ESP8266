b9e8bd2124976b2353231194571aadcb1369445c
Saqibur <saqibur.rahman98@gmail.com>
2019-11-22 17:46:43 +0600

    Change some AP setup configs, now has concerete IP

--- DeviceCode/includes/APSetup.h
+++ DeviceCode/includes/APSetup.h
@@ -1,13 +1,18 @@
 #include <ESP8266WiFi.h>
 void setupAP(char *APssid, char *APpassword)
 {
+    // Maximum of 4 connections allowed.
     Serial.println();
     Serial.print("-----Creating Access Point-----");
 
+    IPAddress local_IP(192,168,135,135);
+    IPAddress gateway(192,168,4,9);
+    IPAddress subnet(255,255,255,0);
+
     WiFi.mode(WIFI_AP);
-    WiFi.softAP(APssid, APpassword);
+    WiFi.softAPConfig(local_IP, gateway, subnet);
+    WiFi.softAP(APssid, APpassword, 1, false, 8);
 
     Serial.println("-----Access Point Created-----");
-
     return;
 }
